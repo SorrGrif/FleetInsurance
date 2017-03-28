d1cccf987e4ea6256e149c3c07e3e26d31b94448
Paul Faucher <paul.faucher8@stclairconnect.ca>
2017-03-28 13:54:21 -0400

    Started the database class, added the string values for the table, database and column names.

---
+++ FleetInsurance/app/src/main/java/faucher/paul/fleetinsurance/DatabaseHandler.java
@@ -0,0 +1,28 @@
+package faucher.paul.fleetinsurance;
+
+public class DatabaseHandler {
+
+    //Database Version
+    private static final int DATABASE_VERSION = 1;
+
+    //Database Name
+    private static final String DATABASE_NAME = "fleetInsurance";
+
+    //Table Names
+    private static final String TABLE_USER = "user";
+    private static final String TABLE_CLAIMS = "claims";
+
+    //Common Column Names
+    private static final String COLUMN_ID = "id";
+
+    //User Table Column Names
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_ADDRESS = "address";
+    private static final String COLUMN_PHONE_NUM = "phone number";
+    private static final String COLUMN_CLAIM_STATUS = "claim_status";
+    private static final String COLUMN_PLAN_STATUS = "plan_status";
+    private static final String COLUMN_PROFILE_PIC = "id_profile_picture";
+
+    //Claim Table Column Names
+    
+}
