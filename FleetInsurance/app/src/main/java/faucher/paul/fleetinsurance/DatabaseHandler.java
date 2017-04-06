df335b0d18c797d9417eed5e92192d95e2f918a9
Paul Faucher <paul.faucher8@stclairconnect.ca>
2017-04-06 09:30:01 -0400

    Added collum names for Claim and User tables, Added SQL statement to create tables.

--- FleetInsurance/app/src/main/java/faucher/paul/fleetinsurance/DatabaseHandler.java
+++ FleetInsurance/app/src/main/java/faucher/paul/fleetinsurance/DatabaseHandler.java
@@ -11,18 +11,32 @@ public class DatabaseHandler {
     //Table Names
     private static final String TABLE_USER = "user";
     private static final String TABLE_CLAIMS = "claims";
+    private static final String TABLE_IMAGES =  "images";
 
     //Common Column Names
-    private static final String COLUMN_ID = "id";
+    private static final String KEY_ID = "id";
 
     //User Table Column Names
-    private static final String COLUMN_NAME = "name";
-    private static final String COLUMN_ADDRESS = "address";
-    private static final String COLUMN_PHONE_NUM = "phone number";
-    private static final String COLUMN_CLAIM_STATUS = "claim_status";
-    private static final String COLUMN_PLAN_STATUS = "plan_status";
-    private static final String COLUMN_PROFILE_PIC = "id_profile_picture";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_ADDRESS = "address";
+    private static final String KEY_PHONE_NUM = "phone_number";
+    private static final String KEY_CLAIM_STATUS = "claim_status";
+    private static final String KEY_PLAN_STATUS = "plan_status";
+    private static final String KEY_PROFILE_PIC = "id_profile_picture";
 
     //Claim Table Column Names
+    private static final String KEY_CLAIM_NAME = "claim_name";
+    private static final String KEY_DATE = "claim_date";
+    private static final String KEY_DESC = "claim_desc";
+    private static final String KEY_PIC = "id_claim_picture";
     
+    //Creating statements to create our tables
+    private static final String CREATE_USER_TABLE = "CREATE TABLE " + TABLE_USER +
+            "(" + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT," + KEY_ADDRESS +
+            " TEXT," + KEY_PHONE_NUM + " TEXT," + KEY_CLAIM_STATUS + " TEXT," + KEY_PLAN_STATUS
+            + " TEXT," + KEY_PROFILE_PIC + " TEXT)";
+
+    private static final String CREATE_CLAIM_TABLE = "CREATE TABLE " + TABLE_CLAIMS +
+            "(" + KEY_ID + " INTERGER PRIMARY KEY," + KEY_CLAIM_NAME + " TEXT," + KEY_DATE
+            + " TEXT," + KEY_DESC + " TEXT," + KEY_PIC + " TEXT)";
 }
